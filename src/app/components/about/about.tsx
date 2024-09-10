f46fbccc94d4761d0a84353e39f04d6e66b24888
Facundo Naranjo <facundo.naranjo00@gmail.com>
2024-09-10 08:11:51 -0300

    created about section

---
+++ src/app/components/about/about.tsx
@@ -0,0 +1,34 @@
+import { aboutMeItems } from './utils';
+import ItemSection from './components/itemSection';
+import { Sections } from '../navbar/utils';
+
+export default function About() {
+  return (
+    <section
+      className="flex flex-col justify-center items-center"
+      id={Sections.ABOUT}
+    >
+      <div className="pb-3 flex flex-col justify-center items-center">
+        <h2 className="font-semibold text-2xl md:text-3xl xl:text-2xl">
+          About me
+        </h2>
+        <p className="text-gray-40">Know me better</p>
+      </div>
+
+      <p className="text-sm md:text-lg xl:text-sm text-white-50 text-center px-8 xl:px-64">
+        With a strong determination to achieve excellent results and a focus on
+        best practices, I build complex systems every day that provide
+        solutions. I was raised with the mindset that nothing is impossible and
+        that everything comes in due time.
+      </p>
+
+      <div className="my-14">
+        <ItemSection items={aboutMeItems.slice(0, 3)} lastElements={false} />
+
+        <div className="hidden xl:flex h-[0.5px] w-full bg-gray-12 my-9" />
+
+        <ItemSection items={aboutMeItems.slice(3, 6)} lastElements />
+      </div>
+    </section>
+  );
+}
---
+++ src/app/components/about/utils.ts
@@ -0,0 +1,47 @@
+import { aboutMeIcons } from '../../../../public/images/aboutMe/aboutMeIcons';
+
+export const aboutMeItems = [
+  {
+    title: 'Soft Skills That Facilitate Problem Solving',
+    description:
+      'To achieve goals, it is essential to master and continuously learn soft skills. My strong soft skills help me create a positive work environment where everyone can comfortably work together to get things done.',
+    icon: aboutMeIcons.softSkills,
+    iconAlt: 'An icon depicting three people.',
+  },
+  {
+    title: 'A Brilliant Mind for Tackling Complex Challenges',
+    description:
+      'I excel at solving complex problems and enjoy the process. I believe every issue has a solution, so I stay focused and calm to find it, turning challenges into opportunities for growth and learning.',
+    icon: aboutMeIcons.brilliantMind,
+    iconAlt:
+      'A white icon of a light bulb with a lightning bolt in the center.',
+  },
+  {
+    title: 'Quick Learner and Adaptable Problem-Solver',
+    description:
+      'I quickly master new skills and reinforce those I have already learned. I enjoy learning from my colleagues and believe there is always room for improvement, which drives me to keep growing and evolving.',
+    icon: aboutMeIcons.fastLearner,
+    iconAlt: 'An icon of a white book.',
+  },
+  {
+    title: 'Programming Enthusiast Since Age 8',
+    description:
+      'I learned to program at 8 years old. My passion for learning and creativity from a young age led me here. I enjoy creating things, and programming lets me channel that creativity.',
+    icon: aboutMeIcons.earlyCommitment,
+    iconAlt: 'A white chart showing growth.',
+  },
+  {
+    title: 'Strength Through Resilience in Challenges',
+    description:
+      'One key to growth is stumbling and getting back up. Learning from mistakes and trying again, better, is the key to success. They say a winner is just a loser who tried one more time.',
+    icon: aboutMeIcons.resilence,
+    iconAlt: 'A white chart showing growth.',
+  },
+  {
+    title: 'Committed to Precision and Excellence',
+    description:
+      'What happens if we ignore a small detail? Probably nothing. But if we overlook many small details, the quality of what we are creating may suffer. That is why I focus on details, they make a difference.',
+    icon: aboutMeIcons.attentionToDetail,
+    iconAlt: 'A white chart showing growth.',
+  },
+];
