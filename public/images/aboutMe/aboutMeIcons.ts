be7004e60c9b73046bd96628a36d98c9f59b15d5
Facundo Naranjo <facundo.naranjo00@gmail.com>
2024-09-10 08:06:12 -0300

    added new icons for about section

---
+++ public/images/aboutMe/aboutMeIcons.ts
@@ -0,0 +1,17 @@
+import attentionToDetail from './attentionToDetail.svg';
+import brilliantMind from './brilliantMind.svg';
+import earlyCommitment from './earlyCommitment.svg';
+import fastLearner from './fastLearner.svg';
+import resilence from './resilence.svg';
+import softSkills from './softSkills.svg';
+import iconContainer from './iconContainer.svg';
+
+export const aboutMeIcons = {
+  attentionToDetail,
+  brilliantMind,
+  earlyCommitment,
+  fastLearner,
+  resilence,
+  softSkills,
+  iconContainer,
+};
