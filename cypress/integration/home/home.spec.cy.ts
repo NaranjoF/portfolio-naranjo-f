47826fda718583ddee7a521ac86490f82dfe99c3
Facundo Naranjo <facundo.naranjo00@gmail.com>
2024-10-04 10:35:48 -0300

    update home test

--- cypress/integration/home/home.spec.cy.ts
+++ cypress/integration/home/home.spec.cy.ts
@@ -11,4 +11,11 @@ describe('Home section', () => {
     cy.get('button').contains('Go to About').click();
     cy.url().should('eq', `${Cypress.config('baseUrl')}#About`);
   });
+
+  it('should redirect the user to the experience section', () => {
+    cy.visit('/');
+
+    cy.get('button').contains('Go to Experience').click();
+    cy.url().should('eq', `${Cypress.config('baseUrl')}#Experience`);
+  });
 });
